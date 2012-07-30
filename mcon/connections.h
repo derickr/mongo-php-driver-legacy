f9ee0b5f513c1d90ddf1f4cc749f52831a6592d6
Derick Rethans <github@derickrethans.nl>
2012-07-30 13:38:59 +0100

    Better error reporting for standalone connections.

--- mcon/connections.h
+++ mcon/connections.h
@@ -3,7 +3,7 @@
 
 #include "types.h"
 
-mongo_connection *mongo_connection_create(mongo_con_manager *manager, mongo_server_def *server_def);
+mongo_connection *mongo_connection_create(mongo_con_manager *manager, mongo_server_def *server_def, char **error_message);
 
 inline int mongo_connection_get_reqid(mongo_connection *con);
 int mongo_connection_ping(mongo_con_manager *manager, mongo_connection *con);
